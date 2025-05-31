3b4878d094ae92447bbe2bed0ffa7ff520a14b4a
techn-0 <gnlrjswjd@naver.com>
2025-05-31 16:06:33 +0900

    내 성 체력바 구현

--- Assets/Scripts/PlayerBase.cs
+++ Assets/Scripts/PlayerBase.cs
@@ -6,21 +6,35 @@ public class PlayerBase : MonoBehaviour
     public int maxHealth = 1000;
     [SerializeField] private int currentHealth;
 
+    [Header("체력바 오브젝트")]
+    public Transform healthBarForeground; // 포그라운드 스프라이트의 Transform
+
     private void Awake()
     {
         currentHealth = maxHealth;
+        UpdateHealthBar();
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+        if (currentHealth < 0) currentHealth = 0;
+        UpdateHealthBar();
         if (currentHealth <= 0)
             DestroyBase();
     }
 
+    private void UpdateHealthBar()
+    {
+        if (healthBarForeground != null)
+        {
+            float ratio = (float)currentHealth / maxHealth;
+            healthBarForeground.localScale = new Vector3(ratio, 1f, 1f);
+        }
+    }
+
     private void DestroyBase()
     {
-        // 기지가 파괴되었을 때 일어날 이벤트 처리(게임 종료 등)
         Destroy(gameObject);
     }
 }
